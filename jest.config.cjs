9d75c10c72f0a21670fa6e5fd2e8e1d6698df07e
ALU-Kid <i.rutazinda@alustudent.com>
2025-06-19 16:51:31 +0200

    test: add jest tests for AirtableClient

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
